8a13d8b52ab4283c0a2d34f52f0ae9be5ef47a00
JuanTLA16 <125713407+JuanTLA16@users.noreply.github.com>
2023-04-01 07:43:50 -0500

    Add files via upload

---
+++ Suma-N-Enteros.py
@@ -0,0 +1,17 @@
+
+
+
+
+
+N = int(input("Digite el valor de N: "))
+
+i = 1
+Suma = 0
+
+while (i<=N):
+    Suma = Suma + i
+    i = i + 1
+    print("La suma es " + str(i))
+    print("La suma es " + str(Suma))
+
+print("La suma es " + str(Suma))
